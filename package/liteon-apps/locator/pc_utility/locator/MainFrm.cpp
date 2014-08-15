0f3cd5ac7c82d2f0d2068e2712b12e5a3d2e8896
liutianpingyanxiaoyu <ltp1986226@126.com>
2014-08-15 08:12:20 -0700

    hidded remote config of pc locator

--- package/liteon-apps/locator/pc_utility/locator/MainFrm.cpp
+++ package/liteon-apps/locator/pc_utility/locator/MainFrm.cpp
@@ -35,8 +35,8 @@ static UINT ToolIndicators[] =
 	ID_DEVICE_INFO,
 //	ID_SEPARATOR,
 	ID_WEB,
-	ID_REBOOT,
-	ID_REMOTE_CONFIG
+	ID_REBOOT
+	//ID_REMOTE_CONFIG
 };
 
 static UINT indicators[] =
@@ -148,8 +148,8 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	str.LoadString(IDS_REBOOT);
 	m_wndToolBar.SetButtonText(4, str);
 
-	str.LoadString(IDS_REMOTE_CONFIG);
-	m_wndToolBar.SetButtonText(5, str);
+	//str.LoadString(IDS_REMOTE_CONFIG);
+	//m_wndToolBar.SetButtonText(5, str);
 	
 	CRect rectToolBar;
 
